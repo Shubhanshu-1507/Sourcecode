15f37c53b1ade0a86f4ec1e5a6feda8dacbbc114
Restyled.io <commits@restyled.io>
2022-10-30 15:48:21 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/5-AddSubMultDivUserDefinedNumber.c
+++ C_Basic_Problems/Sheet_1/5-AddSubMultDivUserDefinedNumber.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-    float a, b;
-    printf("Enter A = \n");
-    scanf("%f", &a);
-    printf("Enter B = \n");
-    scanf("%f", &b);
+int main() {
+  float a, b;
+  printf("Enter A = \n");
+  scanf("%f", &a);
+  printf("Enter B = \n");
+  scanf("%f", &b);
 
-    // Integer  Values
-    printf("EXECUTION in INTEGER\n\n");
+  // Integer  Values
+  printf("EXECUTION in INTEGER\n\n");
 
-    printf("Addition of %d and %d is %d\n", a, b, a + b);
-    printf("Subtraction of %d and %d is %d\n", a, b, a - b);
-    printf("Product of %d and %d is %d\n", a, b, a * b);
-    printf("Division of %d and %d is %d\n\n", a, b, a / b);
+  printf("Addition of %d and %d is %d\n", a, b, a + b);
+  printf("Subtraction of %d and %d is %d\n", a, b, a - b);
+  printf("Product of %d and %d is %d\n", a, b, a * b);
+  printf("Division of %d and %d is %d\n\n", a, b, a / b);
 
-    // float Values
+  // float Values
 
-    printf("EXECUTION in FLOAT\n\n");
+  printf("EXECUTION in FLOAT\n\n");
 
-    printf("Addition of %f and %f is %.2f\n", a, b, a + b);
-    printf("Subtraction of %f and %f is %.2f\n", a, b, a - b);
-    printf("Product of %f and %f is %.2f\n", a, b, a * b);
-    printf("Division of %f and %f is %.2f\n", a, b, a / b);
+  printf("Addition of %f and %f is %.2f\n", a, b, a + b);
+  printf("Subtraction of %f and %f is %.2f\n", a, b, a - b);
+  printf("Product of %f and %f is %.2f\n", a, b, a * b);
+  printf("Division of %f and %f is %.2f\n", a, b, a / b);
 
-    return 0;
+  return 0;
 }
