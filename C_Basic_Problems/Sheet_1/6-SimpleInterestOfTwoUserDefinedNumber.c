3afa6a94b860627556e9c94087f09d8104c111e7
Restyled.io <commits@restyled.io>
2022-10-30 15:52:07 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/6-SimpleInterestOfTwoUserDefinedNumber.c
+++ C_Basic_Problems/Sheet_1/6-SimpleInterestOfTwoUserDefinedNumber.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-    float p, r, t, ans = 0;
-    scanf("%f%f%f", &p, &r, &t);
-    ans = (p * r * t) / 100;
-    printf("%f", ans);
-    return 0;
+int main() {
+  float p, r, t, ans = 0;
+  scanf("%f%f%f", &p, &r, &t);
+  ans = (p * r * t) / 100;
+  printf("%f", ans);
+  return 0;
 }
