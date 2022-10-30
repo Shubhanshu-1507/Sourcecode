5ab224478af4f1725bf973114c762f1e335eb4c6
Restyled.io <commits@restyled.io>
2022-10-30 16:04:55 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/8-FindASCII.c
+++ C_Basic_Problems/Sheet_1/8-FindASCII.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char c;
-    scanf("%c",&c);
-    printf("ASCII Value of %c is %d",c,c);
+  char c;
+  scanf("%c", &c);
+  printf("ASCII Value of %c is %d", c, c);
 }
