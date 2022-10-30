13ee249b7590f3f0d56d52807d2b52ab91c0b7c6
Restyled.io <commits@restyled.io>
2022-10-30 16:07:02 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/9-CubeOfUserDefinedNumber.c
+++ C_Basic_Problems/Sheet_1/9-CubeOfUserDefinedNumber.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main()
-{
-    int a;
-    scanf("%d", &a);
-    printf("Cube of %d is %d", a, a * a * a);
-    return 0;
+int main() {
+  int a;
+  scanf("%d", &a);
+  printf("Cube of %d is %d", a, a * a * a);
+  return 0;
 }
