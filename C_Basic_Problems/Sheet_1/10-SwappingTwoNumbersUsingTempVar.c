1868d7f4ab218b622363bfa9492de42806939f7c
Restyled.io <commits@restyled.io>
2022-10-30 16:08:40 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/10-SwappingTwoNumbersUsingTempVar.c
+++ C_Basic_Problems/Sheet_1/10-SwappingTwoNumbersUsingTempVar.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b, temp;
-    scanf("%d%d",&a,&b);
-    temp = a;
-    a = b;
-    b = temp;
-    printf("A= %d and B = %d\n",a,b);
+int main() {
+  int a, b, temp;
+  scanf("%d%d", &a, &b);
+  temp = a;
+  a = b;
+  b = temp;
+  printf("A= %d and B = %d\n", a, b);
 }
