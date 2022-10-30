300e56f46121924ef6177585464b7494b350f14b
Restyled.io <commits@restyled.io>
2022-10-30 15:41:36 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/4-DivisionOfTwoUserDefinedNumber.c
+++ C_Basic_Problems/Sheet_1/4-DivisionOfTwoUserDefinedNumber.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    float a, b;
-    printf("Enter A = \n");
-    scanf("%f", &a);
-    printf("Enter B = \n");
-    scanf("%f", &b);
+int main() {
+  float a, b;
+  printf("Enter A = \n");
+  scanf("%f", &a);
+  printf("Enter B = \n");
+  scanf("%f", &b);
 
-    printf("Division of %f and %f is %.2f", a, b, a / b);
-    return 0;
+  printf("Division of %f and %f is %.2f", a, b, a / b);
+  return 0;
 }
