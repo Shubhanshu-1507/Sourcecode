6fcc164b1c274207970ecf2d45374901df2f579a
Restyled.io <commits@restyled.io>
2022-10-30 15:55:14 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/7-CalculateAverageAndPercentageOfUserDefinedNumbers.c
+++ C_Basic_Problems/Sheet_1/7-CalculateAverageAndPercentageOfUserDefinedNumbers.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-    float a, b, average = 0, percentage = 0;
-    scanf("%f%f", &a, &b);
-    average = a + b / 2;
-    percentage = (a / b) * 100;
-    printf("Average = %f\n", average);
-    printf("Percentage = %f", percentage);
-    return 0;
+int main() {
+  float a, b, average = 0, percentage = 0;
+  scanf("%f%f", &a, &b);
+  average = a + b / 2;
+  percentage = (a / b) * 100;
+  printf("Average = %f\n", average);
+  printf("Percentage = %f", percentage);
+  return 0;
 }
