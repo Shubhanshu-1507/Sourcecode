1231972e4f38c6faf9d7ddd1bb175a6d4d0fd433
Restyled.io <commits@restyled.io>
2022-10-30 15:37:19 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_1/2-SubtractionOfTwoUserNumber.c
+++ C_Basic_Problems/Sheet_1/2-SubtractionOfTwoUserNumber.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b;
-    printf("Enter A = \n");
-    scanf("%d", &a);
-    printf("Enter B = \n");
-    scanf("%d", &b);
+int main() {
+  int a, b;
+  printf("Enter A = \n");
+  scanf("%d", &a);
+  printf("Enter B = \n");
+  scanf("%d", &b);
 
-    printf("Difference of %d and %d is %d", a, b, a - b);
-    return 0;
+  printf("Difference of %d and %d is %d", a, b, a - b);
+  return 0;
 }
