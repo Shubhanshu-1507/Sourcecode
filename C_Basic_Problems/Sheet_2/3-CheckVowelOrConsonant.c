6cd502d4b60efea255e33a329db5bade71ddb15b
Restyled.io <commits@restyled.io>
2022-10-31 16:20:44 +0000

    Restyled by astyle

--- C_Basic_Problems/Sheet_2/3-CheckVowelOrConsonant.c
+++ C_Basic_Problems/Sheet_2/3-CheckVowelOrConsonant.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     char c;
     int small,big;
     scanf("%c",&c);
-small = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    small = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 
-      big = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-    if(small||big){
+    big = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    if(small||big) {
         printf("%c is a Vowel",c);
     }
-    else{
+    else {
         printf("%c is a Consonant",c);
     }
 }
