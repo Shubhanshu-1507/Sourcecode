3bac8d2eea94f37be9c4b737e17cefaa3b55dd50
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 21:58:15 +0530

    Create 5-ProgramToRelate.c
    5-ProgramToRelate.c

---
+++ 5-ProgramToRelate.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    if(a>b){
+        printf("%d is greater",a);
+    }
+    else if(a<b){
+        printf("%d is greater",b);
+    }
+    else{
+        printf("%d and %d are Equal",a,b);
+    }
+}
