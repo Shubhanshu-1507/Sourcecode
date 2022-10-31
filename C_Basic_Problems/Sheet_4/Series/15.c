7a523f6c2f2453b9e5d8f1ea043767c65fd348c4
Restyled.io <commits@restyled.io>
2022-10-31 17:28:40 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/Series/15.c
+++ C_Basic_Problems/Sheet_4/Series/15.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-int main()
-{
-    int n, i;
-    double sum = 0;
-    printf("enter a num\n");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
-    {
-        sum = sum + (double)i / (i + 1);
-    }
-    printf("sum of series upto %d is %lf", n, sum);
-    return 0;
+int main() {
+  int n, i;
+  double sum = 0;
+  printf("enter a num\n");
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++) {
+    sum = sum + (double)i / (i + 1);
+  }
+  printf("sum of series upto %d is %lf", n, sum);
+  return 0;
 }
