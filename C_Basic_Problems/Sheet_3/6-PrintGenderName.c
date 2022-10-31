fd4f5e80ff14012b994ea58606eeae582dff1b61
Restyled.io <commits@restyled.io>
2022-10-31 17:19:28 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_3/6-PrintGenderName.c
+++ C_Basic_Problems/Sheet_3/6-PrintGenderName.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
-int main()
-{
-    char gender;
-    printf("Enter You Gender: M/F:\n");
-    scanf("%c", &gender);
-    switch (gender)
-    {
-    case 'M':
-        printf("Male");
-        break;
-    case 'm':
-        printf("Male");
-        break;
-    case 'F':
-        printf("Female");
-        break;
-    case 'f':
-        printf("Female");
-        break;
-    default:
-        printf("Invalid Input");
-        break;
-    }
+int main() {
+  char gender;
+  printf("Enter You Gender: M/F:\n");
+  scanf("%c", &gender);
+  switch (gender) {
+  case 'M':
+    printf("Male");
+    break;
+  case 'm':
+    printf("Male");
+    break;
+  case 'F':
+    printf("Female");
+    break;
+  case 'f':
+    printf("Female");
+    break;
+  default:
+    printf("Invalid Input");
+    break;
+  }
 }
