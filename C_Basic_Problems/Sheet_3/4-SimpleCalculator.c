dae4a8ba6570e539a9283bd22b31bbfcd4a3c277
Restyled.io <commits@restyled.io>
2022-10-31 17:15:57 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_3/4-SimpleCalculator.c
+++ C_Basic_Problems/Sheet_3/4-SimpleCalculator.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-int main()
-{
-    int a, b, operator, answer = 0;
-    printf("Enter Two Numbers A and B:\n");
-    scanf("%d", &a);
-    scanf("%d", &b);
+int main() {
+  int a, b, operator, answer = 0;
+  printf("Enter Two Numbers A and B:\n");
+  scanf("%d", &a);
+  scanf("%d", &b);
 
-    printf("Choose One Operator :\n1 Addition\n2 Subtraction\n3 Product\n4 Divide\n");
-    scanf("%d", &operator);
+  printf("Choose One Operator :\n1 Addition\n2 Subtraction\n3 Product\n4 "
+         "Divide\n");
+  scanf("%d", &operator);
 
-    switch (operator)
-    {
-    case 1:
-        printf("Addition of %d and %d is %d", a, b, a + b);
-        break;
-    case 2:
-        printf("Subtraction of %d and %d is %d", a, b, a - b);
-        break;
-    case 3:
-        printf("Product of %d and %d is %d", a, b, a * b);
-        break;
-    case 4:
-        printf("Division of %d and %d is %d", a, b, a / b);
-        break;
+  switch (operator) {
+  case 1:
+    printf("Addition of %d and %d is %d", a, b, a + b);
+    break;
+  case 2:
+    printf("Subtraction of %d and %d is %d", a, b, a - b);
+    break;
+  case 3:
+    printf("Product of %d and %d is %d", a, b, a * b);
+    break;
+  case 4:
+    printf("Division of %d and %d is %d", a, b, a / b);
+    break;
 
-    default:
-        printf("Invalid Selection of Operator");
-        break;
-    }
+  default:
+    printf("Invalid Selection of Operator");
+    break;
+  }
 }
