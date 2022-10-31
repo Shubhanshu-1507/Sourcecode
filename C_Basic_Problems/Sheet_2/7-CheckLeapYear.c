061b463675b183b96e69f9c79d9007fa6fae56e4
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 22:24:18 +0530

    Create  7-CheckLeapYear.c
    7-CheckLeapYear.c

---
+++ C_Basic_Problems/Sheet_2/7-CheckLeapYear.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if ((n % 100) && (n % 4))
+    {
+        printf("%d is not a leap Year", n);
+    }
+    else
+    {
+        printf("%d is a leap Year", n);
+    }
+}
