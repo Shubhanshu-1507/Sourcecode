01a70cbb004957ba8da5ddad83aa37f9d368539a
Restyled.io <commits@restyled.io>
2022-10-31 16:29:57 +0000

    Restyled by clang-format

--- 5-ProgramToRelate.c
+++ 5-ProgramToRelate.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a,b;
-    scanf("%d",&a);
-    scanf("%d",&b);
+  int a, b;
+  scanf("%d", &a);
+  scanf("%d", &b);
 
-    if(a>b) {
-        printf("%d is greater",a);
-    }
-    else if(a<b) {
-        printf("%d is greater",b);
-    }
-    else {
-        printf("%d and %d are Equal",a,b);
-    }
+  if (a > b) {
+    printf("%d is greater", a);
+  } else if (a < b) {
+    printf("%d is greater", b);
+  } else {
+    printf("%d and %d are Equal", a, b);
+  }
 }
