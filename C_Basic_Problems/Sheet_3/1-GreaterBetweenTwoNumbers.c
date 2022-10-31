66c8f4df344aa0fe19ba493f41b8ebaf1b558f96
Restyled.io <commits@restyled.io>
2022-10-31 17:05:21 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_3/1-GreaterBetweenTwoNumbers.c
+++ C_Basic_Problems/Sheet_3/1-GreaterBetweenTwoNumbers.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
+int main() {
+  int a, b;
+  scanf("%d", &a);
+  scanf("%d", &b);
 
-    (a > b) ? printf("A = %d is Greater\n", a) : printf("B = %d is Greater\n", b);
+  (a > b) ? printf("A = %d is Greater\n", a) : printf("B = %d is Greater\n", b);
 }
