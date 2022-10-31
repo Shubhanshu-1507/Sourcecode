b69362c516bd6071d6ec9dc60c0121e9249974f1
Restyled.io <commits@restyled.io>
2022-10-31 16:52:41 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_2/6-GreaterOfThree.c
+++ C_Basic_Problems/Sheet_2/6-GreaterOfThree.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
+int main() {
+  int a, b, c;
+  scanf("%d%d%d", &a, &b, &c);
 
-    if (a >= b && a >= c)
-        printf("A = %d is the largest ", a);
+  if (a >= b && a >= c)
+    printf("A = %d is the largest ", a);
 
-    if (b >= a && b >= c)
-        printf("B = %d is the largest ", b);
+  if (b >= a && b >= c)
+    printf("B = %d is the largest ", b);
 
-    if (c >= a && c >= b)
-        printf("C = %d is the largest", c);
+  if (c >= a && c >= b)
+    printf("C = %d is the largest", c);
 }
