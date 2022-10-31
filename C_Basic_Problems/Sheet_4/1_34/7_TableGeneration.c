0e9597a3ec57f8ae01c1b3b641bd7482403d6431
Restyled.io <commits@restyled.io>
2022-10-31 18:21:59 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/1_34/7_TableGeneration.c
+++ C_Basic_Problems/Sheet_4/1_34/7_TableGeneration.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-int main()
-{
-    int i = 1, n;
-    printf("enter no:\n");
-    scanf("%d", &n);
-    while (i <= 10)
-    {
-        printf("\n%d * %d = %d", n, i, n * i);
-        i++;
-    }
-    return 0;
+int main() {
+  int i = 1, n;
+  printf("enter no:\n");
+  scanf("%d", &n);
+  while (i <= 10) {
+    printf("\n%d * %d = %d", n, i, n * i);
+    i++;
+  }
+  return 0;
 }
