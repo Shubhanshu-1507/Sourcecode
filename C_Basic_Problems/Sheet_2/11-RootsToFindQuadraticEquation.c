400fbd44bbaa6ac4dbe0d7585ce25772b46ec964
Restyled.io <commits@restyled.io>
2022-10-31 16:59:37 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_2/11-RootsToFindQuadraticEquation.c
+++ C_Basic_Problems/Sheet_2/11-RootsToFindQuadraticEquation.c
@@ -1,27 +1,28 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 int main() {
-    float a,b,c;
-    float discriminant=0,realPart = 0,imagPart=0,root1=0,root2=0;
-    printf(" Enter A, B, C as Ax^2 + Bx + C = 0:\n");
-    scanf("%f%f%f",&a,&b,&c);
-    discriminant = (b*b - 4*a*c);
+  float a, b, c;
+  float discriminant = 0, realPart = 0, imagPart = 0, root1 = 0, root2 = 0;
+  printf(" Enter A, B, C as Ax^2 + Bx + C = 0:\n");
+  scanf("%f%f%f", &a, &b, &c);
+  discriminant = (b * b - 4 * a * c);
 
-    if (discriminant > 0) {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
-        printf("root1 = %f and root2 = %f", root1, root2);
-    }
+  if (discriminant > 0) {
+    root1 = (-b + sqrt(discriminant)) / (2 * a);
+    root2 = (-b - sqrt(discriminant)) / (2 * a);
+    printf("root1 = %f and root2 = %f", root1, root2);
+  }
 
-    else if (discriminant == 0) {
-        root1 = root2 = -b / (2 * a);
-        printf("root1 = root2 = %f\n", root1);
-    }
+  else if (discriminant == 0) {
+    root1 = root2 = -b / (2 * a);
+    printf("root1 = root2 = %f\n", root1);
+  }
 
-    else {
-        realPart = -b / (2 * a);
-        imagPart = sqrt(-discriminant) / (2 * a);
-        printf("root1 = %f+%fi and root2 = %f-%fi\n", realPart, imagPart, realPart, imagPart);
-    }
+  else {
+    realPart = -b / (2 * a);
+    imagPart = sqrt(-discriminant) / (2 * a);
+    printf("root1 = %f+%fi and root2 = %f-%fi\n", realPart, imagPart, realPart,
+           imagPart);
+  }
 }
