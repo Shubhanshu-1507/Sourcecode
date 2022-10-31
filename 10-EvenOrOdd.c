a5722c226089a36789bf97c082cbd19d4d10b3fd
Restyled.io <commits@restyled.io>
2022-10-31 16:41:53 +0000

    Restyled by clang-format

--- 10-EvenOrOdd.c
+++ 10-EvenOrOdd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d",&n);
-    (n&1)?printf("%d is odd",n) :printf("%d is Even",n);
+  int n;
+  scanf("%d", &n);
+  (n & 1) ? printf("%d is odd", n) : printf("%d is Even", n);
 }
