1af058efd67177dc0a5aa2be409a9bc8051764b8
Restyled.io <commits@restyled.io>
2022-10-31 16:16:56 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_2/2-CheckCharacteIsAlphabetOrNot.c
+++ C_Basic_Problems/Sheet_2/2-CheckCharacteIsAlphabetOrNot.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 int main() {
-    char s;
-    scanf("%c",&s);
-    if((s>=65&&s<=90)|| (s>=97&&s<=122)) {
-        printf("%c is a Character",s);
+  char s;
+  scanf("%c", &s);
+  if ((s >= 65 && s <= 90) || (s >= 97 && s <= 122)) {
+    printf("%c is a Character", s);
 
-    }
-    else {
-        printf("%c is not a character",s);
-    }
-
+  } else {
+    printf("%c is not a character", s);
+  }
 }
