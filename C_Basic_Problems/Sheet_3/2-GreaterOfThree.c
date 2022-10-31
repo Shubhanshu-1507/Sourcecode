f2779880e456e63916d1d1e12b62e53f2ac5708b
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 22:37:41 +0530

    Create 2-GreaterOfThree.c
    2-GreaterOfThree.c

---
+++ C_Basic_Problems/Sheet_3/2-GreaterOfThree.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, answer = 0;
+    scanf("%d%d%d", &a, &b, &c);
+
+    answer = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    printf("%d is Greatest of Three ", answer);
+}
