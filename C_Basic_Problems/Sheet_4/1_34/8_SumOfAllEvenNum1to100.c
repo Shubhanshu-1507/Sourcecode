97570cfceb069a1a419ef8e40b4c3f372c1865fe
Restyled.io <commits@restyled.io>
2022-10-31 18:23:27 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/1_34/8_SumOfAllEvenNum1to100.c
+++ C_Basic_Problems/Sheet_4/1_34/8_SumOfAllEvenNum1to100.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-int main()
-{
-    int i = 1, sum = 0;
+int main() {
+  int i = 1, sum = 0;
 
-    while (i <= 100)
-    {
-        if (i % 2 == 0)
-        {
-            sum = sum + i;
-        }
-        i++;
+  while (i <= 100) {
+    if (i % 2 == 0) {
+      sum = sum + i;
     }
-    printf("Sum = %d", sum);
-    return 0;
+    i++;
+  }
+  printf("Sum = %d", sum);
+  return 0;
 }
