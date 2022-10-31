1ce99567bd0e5305d0629622095a98657855307d
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 21:53:30 +0530

    Create 4-CheckPosOrNot.c
    4-CheckPosOrNot.c

---
+++ C_Basic_Problems/Sheet_2/4-CheckPosOrNot.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    scanf("%d",&i);
+    if(i>0){
+        printf("%d is Positive\n",i);
+    }
+    else{
+        printf("%d is Negative\n",i);
+    }
+}
