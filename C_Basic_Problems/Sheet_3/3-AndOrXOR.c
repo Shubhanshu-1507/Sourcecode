70863410bcbdf8eeeffde5bcff7792c2fa22d129
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 22:41:59 +0530

    Create  3-AndOrXOR.c
    3-AndOrXOR.c

---
+++ C_Basic_Problems/Sheet_3/3-AndOrXOR.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, answer_AND, answer_OR, answer_XOR;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    answer_AND = a & b; // AND
+    answer_OR = a | b;  // OR
+    answer_XOR = a ^ b; // XOR
+
+    printf("AND of %d and %d = %d\n", a, b, answer_AND);
+    printf("OR of %d and %d = %d\n", a, b, answer_OR);
+    printf("XOR of %d and %d = %d\n", a, b, answer_XOR);
+}
