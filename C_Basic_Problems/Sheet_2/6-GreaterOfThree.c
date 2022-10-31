d814228220c9226794ba5e78a5c073cef76b5ccf
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 22:21:51 +0530

    Create 6-GreaterOfThree.c
    6-GreaterOfThree.c

---
+++ C_Basic_Problems/Sheet_2/6-GreaterOfThree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (a >= b && a >= c)
+        printf("A = %d is the largest ", a);
+
+    if (b >= a && b >= c)
+        printf("B = %d is the largest ", b);
+
+    if (c >= a && c >= b)
+        printf("C = %d is the largest", c);
+}
