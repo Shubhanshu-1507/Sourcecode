8bf935e759402cbbd6ba050c5e15b73a102b9f0b
Restyled.io <commits@restyled.io>
2022-10-31 17:32:36 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/Series/17.c
+++ C_Basic_Problems/Sheet_4/Series/17.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-int main()
-{
-    int n, x, i, j, z;
-    double sum = 1.0;
-    printf("enter value of x and n\n");
-    scanf("%d%d", &x, &n);
-    for (i = 1; i <= n; i++)
-    {
-        z = 1;
-        for (j = 1; j <= i; j++)
-        {
-            z = z * x;
-        }
-        if (i % 2 == 0)
-            sum = sum + z;
-        else
-            sum = sum - z;
+int main() {
+  int n, x, i, j, z;
+  double sum = 1.0;
+  printf("enter value of x and n\n");
+  scanf("%d%d", &x, &n);
+  for (i = 1; i <= n; i++) {
+    z = 1;
+    for (j = 1; j <= i; j++) {
+      z = z * x;
     }
-    printf("sum of series upto %d is %lf", n, sum);
-    return 0;
+    if (i % 2 == 0)
+      sum = sum + z;
+    else
+      sum = sum - z;
+  }
+  printf("sum of series upto %d is %lf", n, sum);
+  return 0;
 }
