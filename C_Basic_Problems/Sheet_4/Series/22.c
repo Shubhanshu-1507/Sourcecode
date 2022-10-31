acbc0e47d366913e43d1466f21155cd71bc6c073
Restyled.io <commits@restyled.io>
2022-10-31 17:43:35 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/Series/22.c
+++ C_Basic_Problems/Sheet_4/Series/22.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-int main()
-{
-    int n, x, i, j, k, z, fact;
-    double sum = 1.0;
-    printf("enter value of x and n\n");
-    scanf("%d%d", &x, &n);
-    for (i = 2; i <= n; i = i + 2)
-    {
-        z = 1;
-        fact = 1;
-        for (j = 1; j <= i; j++)
-        {
-            z = z * x;
-        }
-        for (k = 1; k <= i; k++)
-        {
-            fact = fact * k;
-        }
-        sum = sum + (double)z / fact;
+int main() {
+  int n, x, i, j, k, z, fact;
+  double sum = 1.0;
+  printf("enter value of x and n\n");
+  scanf("%d%d", &x, &n);
+  for (i = 2; i <= n; i = i + 2) {
+    z = 1;
+    fact = 1;
+    for (j = 1; j <= i; j++) {
+      z = z * x;
     }
-    printf("sum of series upto %d is %lf", n, sum);
-    return 0;
+    for (k = 1; k <= i; k++) {
+      fact = fact * k;
+    }
+    sum = sum + (double)z / fact;
+  }
+  printf("sum of series upto %d is %lf", n, sum);
+  return 0;
 }
