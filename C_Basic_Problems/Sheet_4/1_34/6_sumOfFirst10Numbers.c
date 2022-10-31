f5597788a334b00fa63bf8d32089c8eddcfb40d4
Restyled.io <commits@restyled.io>
2022-10-31 18:21:10 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/1_34/6_sumOfFirst10Numbers.c
+++ C_Basic_Problems/Sheet_4/1_34/6_sumOfFirst10Numbers.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-int main()
-{
-    int i = 1, sum = 0;
-    while (i <= 10)
-    {
-        sum += i;
-        i++;
-    }
-    printf("Sum = %d", sum);
-    return 0;
+int main() {
+  int i = 1, sum = 0;
+  while (i <= 10) {
+    sum += i;
+    i++;
+  }
+  printf("Sum = %d", sum);
+  return 0;
 }
