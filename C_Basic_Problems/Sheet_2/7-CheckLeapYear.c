259d0ab2c79ea1e4af06133539a71b2ad2ca5f88
Restyled.io <commits@restyled.io>
2022-10-31 16:55:17 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_2/7-CheckLeapYear.c
+++ C_Basic_Problems/Sheet_2/7-CheckLeapYear.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    if ((n % 100) && (n % 4))
-    {
-        printf("%d is not a leap Year", n);
-    }
-    else
-    {
-        printf("%d is a leap Year", n);
-    }
+int main() {
+  int n;
+  scanf("%d", &n);
+  if ((n % 100) && (n % 4)) {
+    printf("%d is not a leap Year", n);
+  } else {
+    printf("%d is a leap Year", n);
+  }
 }
