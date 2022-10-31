c09e697a64bafefe68042d3cc1f3304d9a1b5b3b
Restyled.io <commits@restyled.io>
2022-10-31 18:18:28 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/1_34/3_name10Times.c
+++ C_Basic_Problems/Sheet_4/1_34/3_name10Times.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-int main()
-{
-    int i = 1;
-    do
-    {
-        printf("\nSiddhant Khare");
-        i++;
-    } while (i <= 10);
-    return 0;
+int main() {
+  int i = 1;
+  do {
+    printf("\nSiddhant Khare");
+    i++;
+  } while (i <= 10);
+  return 0;
 }
