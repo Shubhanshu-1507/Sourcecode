c2cd4293fb58f435fe1dc563c121cb2deb9dee2a
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 23:10:30 +0530

    Create 21
    21

---
+++ C_Basic_Problems/Sheet_4/Series/21.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n, x, i, j, k, z, fact;
+    double sum = 1.0;
+    printf("enter value of x and n\n");
+    scanf("%d%d", &x, &n);
+    for (i = 1; i <= n; i = i + 2)
+    {
+        z = 1;
+        fact = 1;
+        for (j = 1; j <= i; j++)
+        {
+            z = z * x;
+        }
+        for (k = 1; k <= i; k++)
+        {
+            fact = fact * k;
+        }
+        sum = sum + (double)z / fact;
+    }
+    printf("sum of series upto %d is %lf", n, sum);
+    return 0;
+}
