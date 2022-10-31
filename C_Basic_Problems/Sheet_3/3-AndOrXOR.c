3405a08efdb2714582905e945bde4c7416a26a83
Restyled.io <commits@restyled.io>
2022-10-31 17:12:51 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_3/3-AndOrXOR.c
+++ C_Basic_Problems/Sheet_3/3-AndOrXOR.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
-    int a, b, answer_AND, answer_OR, answer_XOR;
-    scanf("%d", &a);
-    scanf("%d", &b);
+int main() {
+  int a, b, answer_AND, answer_OR, answer_XOR;
+  scanf("%d", &a);
+  scanf("%d", &b);
 
-    answer_AND = a & b; // AND
-    answer_OR = a | b;  // OR
-    answer_XOR = a ^ b; // XOR
+  answer_AND = a & b; // AND
+  answer_OR = a | b;  // OR
+  answer_XOR = a ^ b; // XOR
 
-    printf("AND of %d and %d = %d\n", a, b, answer_AND);
-    printf("OR of %d and %d = %d\n", a, b, answer_OR);
-    printf("XOR of %d and %d = %d\n", a, b, answer_XOR);
+  printf("AND of %d and %d = %d\n", a, b, answer_AND);
+  printf("OR of %d and %d = %d\n", a, b, answer_OR);
+  printf("XOR of %d and %d = %d\n", a, b, answer_XOR);
 }
