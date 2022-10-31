79c11b1fdc32f177aa1fdc4a982d2489bf232eb6
Restyled.io <commits@restyled.io>
2022-10-31 18:25:12 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/1_34/10_maxAndMin.c
+++ C_Basic_Problems/Sheet_4/1_34/10_maxAndMin.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-int main()
-{
-    int a, i, max, min;
-    printf("enter 10 numbers: \n");
+int main() {
+  int a, i, max, min;
+  printf("enter 10 numbers: \n");
+  scanf("%d", &a);
+  max = a;
+  min = a;
+  for (i = 2; i <= 10; i++) {
     scanf("%d", &a);
-    max = a;
-    min = a;
-    for (i = 2; i <= 10; i++)
-    {
-        scanf("%d", &a);
-        if (a > max)
-        {
-            max = a;
-        }
-        else if (a < min)
-        {
-            min = a;
-        }
+    if (a > max) {
+      max = a;
+    } else if (a < min) {
+      min = a;
     }
-    printf("Maximum=%d and Minimum=%d", max, min);
-    return 0;
+  }
+  printf("Maximum=%d and Minimum=%d", max, min);
+  return 0;
 }
