ad04ffc3ac3a1dadfd357da8f31889612537bbcc
Restyled.io <commits@restyled.io>
2022-10-31 18:24:17 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_4/1_34/9_n^m.c
+++ C_Basic_Problems/Sheet_4/1_34/9_n^m.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-int main()
-{
-    int n, m, pow = 1, i = 1;
-    printf("enter value of Base (n) amd Power (m)\n");
-    scanf("%d%d", &n, &m);
-    while (i <= m)
-    {
-        pow = pow * n;
-        i++;
-    }
-    printf("\n Base(n)^Power(m)=%d", pow);
-    return 0;
+int main() {
+  int n, m, pow = 1, i = 1;
+  printf("enter value of Base (n) amd Power (m)\n");
+  scanf("%d%d", &n, &m);
+  while (i <= m) {
+    pow = pow * n;
+    i++;
+  }
+  printf("\n Base(n)^Power(m)=%d", pow);
+  return 0;
 }
