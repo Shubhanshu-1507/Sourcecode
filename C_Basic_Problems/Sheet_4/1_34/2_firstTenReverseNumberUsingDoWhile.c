b11ce273d1e61372f2d929788353752d74ef8dcd
Shubhanshu Kushwaha <cs1916@global.org.in>
2022-10-31 23:46:27 +0530

    Create 2_firstTenReverseNumberUsingDoWhile.c
    2_firstTenReverseNumberUsingDoWhile.c

---
+++ C_Basic_Problems/Sheet_4/1_34/2_firstTenReverseNumberUsingDoWhile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n = 10;
+
+    printf("Numbers from 1 to 10: \n");
+
+    do
+    {
+        printf("%d\n", n);
+        n--;
+    } while (n >= 1);
+
+    return 0;
+}
