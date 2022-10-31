6ad044e4d61a02d2d855fadaeb435eb6ba2aeebc
Restyled.io <commits@restyled.io>
2022-10-31 16:24:28 +0000

    Restyled by clang-format

--- C_Basic_Problems/Sheet_2/4-CheckPosOrNot.c
+++ C_Basic_Problems/Sheet_2/4-CheckPosOrNot.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    scanf("%d",&i);
-    if(i>0) {
-        printf("%d is Positive\n",i);
-    }
-    else {
-        printf("%d is Negative\n",i);
-    }
+  int i;
+  scanf("%d", &i);
+  if (i > 0) {
+    printf("%d is Positive\n", i);
+  } else {
+    printf("%d is Negative\n", i);
+  }
 }
