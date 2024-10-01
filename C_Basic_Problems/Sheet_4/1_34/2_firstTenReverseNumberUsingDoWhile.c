3ca41db24072c7b0b92d0b7fb8246df318651527
Iqra-hash797 <iqrathanedar3@gmail.com>
2024-10-01 18:25:57 +0530

    First commit

--- C_Basic_Problems/Sheet_4/1_34/2_firstTenReverseNumberUsingDoWhile.c
+++ C_Basic_Problems/Sheet_4/1_34/2_firstTenReverseNumberUsingDoWhile.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
+void printNumber(int n) {
+    printf("%d\n", n);
+}
 
+int main() {
     int n = 10;
+    int numbers[n];
 
-    printf("Numbers from 1 to 10: \n");
+    // Initialize the array with numbers from 1 to 10
+    for (int i = 0; i < n; i++) {
+        numbers[i] = i + 1;
+    }
 
-    do
-    {
-        printf("%d\n", n);
-        n--;
-    } while (n >= 1);
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Shuffle the numbers array using Fisher-Yates algorithm
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        // Swap numbers[i] and numbers[j]
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+
+    // Print the numbers in reverse order
+    printf("Numbers from 10 to 1:\n");
+    for (int i = n - 1; i >= 0; i--) {
+        printNumber(numbers[i]);
+    }
 
     return 0;
 }
+
