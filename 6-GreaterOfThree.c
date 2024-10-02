52797be0d021ca01f1d17c6c7a30b3bf60946f80
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2024-10-02 15:06:43 +0530

    Update 6-GreaterOfThree.c

--- 6-GreaterOfThree.c
+++ 6-GreaterOfThree.c
@@ -1,3 +1,5 @@
+// Method 1:
+
 #include <stdio.h>
 
 int main()
@@ -13,4 +15,20 @@ int main()
 
     if (c >= a && c >= b)
         printf("C = %d is the largest", c);
+}
+
+// Method 2:
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int largest = a;
+
+    if (b > largest) { largest = b; }
+    
+    if (c > largest) { largest = c; }
+
+    printf("The largest number is %d\n", largest);
 }
