8f7b308336e30ce139fad51bc025c9698a995db7
codersunny812 <sengersunny448@gmail.com>
2024-10-02 16:08:22 +0530

    pattern in c

---
+++ pattern/pattern2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, row;
+    printf("enter the no of row:\n");
+    scanf("%d", &row);
+    for (i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
