abed35e53ed76fc790f4508e1e12aea40c958d78
Mustafa1765 <shaikmustafaali1765@gmail.com>
2024-10-03 23:21:52 +0530

    swaping without tempVar

--- C_Basic_Problems/Sheet_1/10-SwappingTwoNumbersUsingTempVar.c
+++ C_Basic_Problems/Sheet_1/10-SwappingTwoNumbersUsingTempVar.c
@@ -4,8 +4,8 @@ int main()
 {
     int a, b, temp;
     scanf("%d%d",&a,&b);
-    temp = a;
-    a = b;
-    b = temp;
+    a = a + b;  
+    b = a - b; 
+    a = a - b;  
     printf("A= %d and B = %d\n",a,b);
 }
