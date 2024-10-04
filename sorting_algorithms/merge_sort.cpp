9a43e1c6c20c5baca54c9fc75a3a7a60a25835f7
Restyled.io <commits@restyled.io>
2024-10-04 11:29:18 +0000

    Restyled by astyle

--- sorting_algorithms/merge_sort.cpp
+++ sorting_algorithms/merge_sort.cpp
@@ -7,7 +7,7 @@ void merge(vector<int>& arr, int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
     vector<int> L(n1), R(n2);
-    
+
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
