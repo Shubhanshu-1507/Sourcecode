c79d44cb1774a86f9049143b4c92f7b0027e6194
Restyled.io <commits@restyled.io>
2024-10-04 11:29:19 +0000

    Restyled by clang-format

--- sorting_algorithms/merge_sort.cpp
+++ sorting_algorithms/merge_sort.cpp
@@ -3,63 +3,63 @@
 using namespace std;
 
 // Function to merge two subarrays
-void merge(vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    vector<int> L(n1), R(n2);
+void merge(vector<int> &arr, int left, int mid, int right) {
+  int n1 = mid - left + 1;
+  int n2 = right - mid;
+  vector<int> L(n1), R(n2);
 
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[left + i];
+  for (int j = 0; j < n2; j++)
+    R[j] = arr[mid + 1 + j];
 
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+  int i = 0, j = 0, k = left;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
     }
+    k++;
+  }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
 
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
 }
 
 // Function to perform Merge Sort
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
+void mergeSort(vector<int> &arr, int left, int right) {
+  if (left < right) {
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+  }
 }
 
 // Function to print the array
-void printArray(const vector<int>& arr) {
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
+void printArray(const vector<int> &arr) {
+  for (int num : arr) {
+    cout << num << " ";
+  }
+  cout << endl;
 }
 
 int main() {
-    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
-    mergeSort(arr, 0, arr.size() - 1);
-    cout << "Sorted array (Merge Sort): ";
-    printArray(arr);
-    return 0;
+  vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+  mergeSort(arr, 0, arr.size() - 1);
+  cout << "Sorted array (Merge Sort): ";
+  printArray(arr);
+  return 0;
 }
