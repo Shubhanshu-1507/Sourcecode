ff4d8180076437a4e67b11441d35256f9600e735
Restyled.io <commits@restyled.io>
2024-10-04 13:07:54 +0000

    Restyled by clang-format

--- sorting_algorithms/insertion_sort.cpp
+++ sorting_algorithms/insertion_sort.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 // Function to perform Insertion Sort
 void insertionSort(vector<int> &arr) {
-    int n = arr.size();
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
+  int n = arr.size();
+  for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = key;
+  }
 }
 
 // Function to print the array
 void printArray(const vector<int> &arr) {
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
+  for (int num : arr) {
+    cout << num << " ";
+  }
+  cout << endl;
 }
 
 int main() {
-    vector<int> arr = {12, 11, 13, 5, 6};
-    insertionSort(arr);
-    cout << "Sorted array (Insertion Sort): ";
-    printArray(arr);
-    return 0;
+  vector<int> arr = {12, 11, 13, 5, 6};
+  insertionSort(arr);
+  cout << "Sorted array (Insertion Sort): ";
+  printArray(arr);
+  return 0;
 }
--- sorting_algorithms/selection_sort.cpp
+++ sorting_algorithms/selection_sort.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 // Function to perform Selection Sort
 void selectionSort(vector<int> &arr) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        swap(arr[i], arr[minIndex]);
+  int n = arr.size();
+  for (int i = 0; i < n - 1; i++) {
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
     }
+    swap(arr[i], arr[minIndex]);
+  }
 }
 
 // Function to print the array
 void printArray(const vector<int> &arr) {
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
+  for (int num : arr) {
+    cout << num << " ";
+  }
+  cout << endl;
 }
 
 int main() {
-    vector<int> arr = {64, 25, 12, 22, 11};
-    selectionSort(arr);
-    cout << "Sorted array (Selection Sort): ";
-    printArray(arr);
-    return 0;
+  vector<int> arr = {64, 25, 12, 22, 11};
+  selectionSort(arr);
+  cout << "Sorted array (Selection Sort): ";
+  printArray(arr);
+  return 0;
 }
