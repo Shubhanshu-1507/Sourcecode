e4d3dedbb3a57ace0eceb16c652aa238b994ad30
Sakethram <saketh1844@gmail.com>
2024-10-07 17:27:02 +0530

    Update Sudoku Solver.cpp

--- CPP/Sudoku Solver.cpp
+++ CPP/Sudoku Solver.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 
@@ -41,17 +41,23 @@ public:
 };
 
 int main() {
-    vector<vector<char>> board(9, vector<char>(9));
-
-    cout << "Enter the Sudoku board (9x9), use '.' for empty cells:" << endl;
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            cin >> board[i][j];
-        }
-    }
+    // Static input: Sudoku puzzle where '.' represents empty cells
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
 
     Solution solution;
     solution.solveSudoku(board);
+
+    // Print the solved Sudoku
     cout << "Solved Sudoku board:" << endl;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
