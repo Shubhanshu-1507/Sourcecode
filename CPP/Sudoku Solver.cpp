9ecd994f7346802aa6b8ed5284029b1830403348
Sakethram <saketh1844@gmail.com>
2024-10-07 17:31:45 +0530

    Update Sudoku Solver.cpp

--- CPP/Sudoku Solver.cpp
+++ CPP/Sudoku Solver.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 #include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-    bool isPoss(int k, int j, vector<vector<char>> &board, char ch) {
+    bool isPoss(int k, int j, vector<vector<char>>& board, char ch) {
         for (int i = 0; i < 9; i++) {
-            if (board[i][j] == ch)
+            if (board[i][j] == ch || board[k][i] == ch || board[3 * (k / 3) + i / 3][3 * (j / 3) + i % 3] == ch) {
                 return false;
-            if (board[k][i] == ch)
-                return false;
-            if (board[3 * (k / 3) + i / 3][3 * (j / 3) + i % 3] == ch)
-                return false;
+            }
         }
         return true;
     }
 
-    bool solV(vector<vector<char>> &board) {
+    bool solV(vector<vector<char>>& board) {
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board.size(); j++) {
                 if (board[i][j] == '.') {
@@ -37,7 +35,7 @@ public:
         return true;
     }
 
-    void solveSudoku(vector<vector<char>> &board) { solV(board); }
+    void solveSudoku(vector<vector<char>>& board) { solV(board); }
 };
 
 int main() {
